5cdfa565ec3927466fa91f7e6faf42a325af9e54
Corey Richardson <corey@octayn.net>
2014-10-12 15:35:50 -0400

    Entities

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
-#[test]
-fn it_works() {
+extern crate time;
+
+use time::Timespec;
+
+pub struct History {
+    pub isbn: String,
+    pub student_id: String,
+    pub date: Timespec,
+    pub action: Action,
+}
+
+pub enum Action {
+    CheckOut,
+    CheckIn,
+}
+
+pub struct User {
+    pub name: String,
+    pub email: String,
+    pub student_id: String,
+}
+
+pub struct Book {
+    pub name: String,
+    pub description: String,
+    pub isbn: String,
+    pub cover_image: Vec<u8>,
+    pub available: i16,
+    pub quantity: i16,
+    pub active_date: Timespec,
+    pub permission: Permission,
+}
+
+pub enum Permission {
+    DontLeaveLibrary,
+    FreeToCheckOut,
 }

24549b3620eac051d93271651ee3daa3d651dc62
Corey Richardson <corey@octayn.net>
2014-10-12 16:52:02 -0400

    Add derivings

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate time;
+extern crate serialize;
 
 use time::Timespec;
 
+#[deriving(Encodable, Decodable, Show)]
 pub struct History {
     pub isbn: String,
     pub student_id: String,
@@ -9,17 +11,20 @@ pub struct History {
     pub action: Action,
 }
 
+#[deriving(Encodable, Decodable, Show, Hash)]
 pub enum Action {
     CheckOut,
     CheckIn,
 }
 
+#[deriving(Encodable, Decodable, Show, Hash)]
 pub struct User {
     pub name: String,
     pub email: String,
     pub student_id: String,
 }
 
+#[deriving(Encodable, Decodable, Show)]
 pub struct Book {
     pub name: String,
     pub description: String,
@@ -31,7 +36,13 @@ pub struct Book {
     pub permission: Permission,
 }
 
+#[deriving(Encodable, Decodable, Show, Hash)]
 pub enum Permission {
     DontLeaveLibrary,
     FreeToCheckOut,
+}
+
+/// Convert from an i16 (SMALLINT in the database) to an enum variant if it's valid
+pub fn enum_from_id<T: FromPrimitive>(id: i16) -> Option<T> {
+    FromPrimitive::from_i16(id)
 }
